7c2fc390d01a99faef56294fbd761831fdf9213a
Louishancquart <hancquartlouis@gmail.com>
2017-01-26 15:51:38 +0100

    vertical bar added on the website (with colors)

--- app/video-box/video-box.component.ts
+++ app/video-box/video-box.component.ts
@@ -6,8 +6,30 @@ import { Component } from '@angular/core';
   moduleId: module.id,
   selector: 'video-box',
   templateUrl: 'video-box.component.html',
-  // styleUrls:['donate.component.css']
+  styleUrls:['video-box.component.css']
 })
+
+// function insertTableIntoPlayer() {
+//   var playerElement=document.getElementById("player");
+//   var parent_Element=playerElement.parentElement;
+//
+//   var x = document.createElement("TABLE");
+//   x.setAttribute("id", "myTable");
+//   parent_Element.appendChild(x);
+//
+//   var y = document.createElement("TR");
+//   y.setAttribute("id", "myTr");
+//   document.getElementById("myTable").appendChild(y);
+//
+//   var z = document.createElement("TD");
+//   z.appendChild(playerElement);
+//   document.getElementById("myTr").appendChild(z);
+//
+//
+//   var z2 = document.createElement("TD");
+//   z2.setAttribute("id", "plugin");
+//   document.getElementById("myTr").appendChild(z2);
+// }
 
 
 export class VideoBoxComponent  {
