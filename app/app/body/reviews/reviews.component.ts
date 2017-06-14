b3175389febc0a697e55bdf16f6368bfbc4fbdae
Louishancquart <hancquartlouis@gmail.com>
2017-06-14 12:16:56 +0200

    add progressbar calculation

--- app/app/body/reviews/reviews.component.ts
+++ app/app/body/reviews/reviews.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 import {DocumentService} from '../../../services/document.service';
 import {RDocument} from '../../../services/data';
@@ -19,6 +19,16 @@ export class ReviewsComponent implements OnInit {
     positiveDocumentList: RDocument[];
     negativeDocumentList: RDocument[];
     controversialDocumentList: RDocument[];
+
+    positiveRelevance: number;
+    negativeRelevance: number;
+    controversialRelevance: number;
+    totalRelevance: number;
+
+    positiveRelevancePercentage: number;
+    negativeRelevancePercentage: number;
+    controversialRelevancePercentage: number;
+
 
     constructor(private documentService: DocumentService) {}
 
@@ -51,9 +61,39 @@ export class ReviewsComponent implements OnInit {
                 this.updateLists();
             });
     }
-    updateLists(): void{
+
+    updateLists(): void {
         this.positiveDocumentList = this.docList.filter(doc => doc.type === 'positive');
         this.controversialDocumentList = this.docList.filter(doc => doc.type === 'controversial');
         this.negativeDocumentList = this.docList.filter(doc => doc.type === 'negative');
+        this.calculateProgress();
+    }
+
+    calculateProgress(): void {
+        this.totalRelevance = this.positiveRelevance = this.negativeRelevance = this.controversialRelevance = 0;
+
+        for (let doc of this.docList) {
+            this.totalRelevance += doc.relevance;
+            switch (doc.type) {
+                case 'positive': {
+                    this.positiveRelevance += doc.relevance;
+                    break;
+                }
+                case 'negative': {
+                    this.negativeRelevance += doc.relevance;
+                    break;
+                }
+                case 'controversial': {
+                    this.controversialRelevance += doc.relevance;
+                    break;
+                }
+                default: {
+                    console.log('rel: ', doc.relevance, 'type:', doc.type, 'total:', this.totalRelevance );
+                }
+            }
+        }
+        this.positiveRelevancePercentage = (this.positiveRelevance / this.totalRelevance) * 100 ;
+        this.negativeRelevancePercentage = (this.negativeRelevance / this.totalRelevance) * 100 ;
+        this.controversialRelevancePercentage = (this.controversialRelevance / this.totalRelevance) * 100;
     }
 }
